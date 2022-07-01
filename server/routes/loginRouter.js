8c7e0da5263750615bd8e9341b370ab58d078cff
Carol Xia <c.xia.98@gmail.com>
2022-06-30 18:44:08 -0700

    login and signup but it doesn't work

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 // const db = require('../models/databaseModel');
 
-// redirect to login page
-router.get('/', (req, res) => {
-    // console.log(res.locals.newUser )
-    return res.status(200).json(`Redirected to login page`);
-});
+// // redirect to login page
+// router.get('/', (req, res) => {
+//     // console.log(res.locals.newUser )
+//     return res.status(200).json(`Redirected to login page`);
+// });
 
 // First checks if user exists
 // If user exists, reroutes to home page
@@ -15,10 +15,10 @@ router.get('/', (req, res) => {
 router.post('/', 
   userController.checkUserInputs, 
   userController.checkUserExists, 
-  userController.redirectToHomePage,
+  // userController.redirectToHomePage,
   (req, res) => {
-    // console.log(res.locals.newUser )
-    return res.status(200).json(`Successfully logged in!`);
+    console.log(res.locals.userExists)
+    return res.status(200).json(res.locals.userExists);
 });
 
 module.exports = router;
